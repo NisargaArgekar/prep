61d0af85b0692e5412a05b4fb1566109ed9c38a0
NisargaArgekar <argekarnisarga015@gmail.com>
2025-07-09 00:18:30 +0530

    anagram

---
+++ anagram.java
@@ -0,0 +1,16 @@
+
+// Brute force solution to check if two strings are anagrams
+
+public class anagram {
+    public static void main(String[] args) {
+        // Check if two strings are anagrams
+        // An anagram is a word or phrase formed by rearranging the letters of a different
+        String s = "tomato";
+        String t = "matoto";
+        if(s.length() != t.length()) {
+            System.out.println("Not anagrams");
+            return;
+        }
+        
+    }
+}
